026c3b24a38807688402e3e0018f6a6ab6239946
posternaka <nik.petrenko97@gmail.com>
2024-05-04 17:03:14 +0300

    developed and styled navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+import Navbar from "./scenes/navbar"
+
 function App() {
-	return <div className='app'>app</div>;
+	const [selectedPage, setSelectedPage] = useState<string>("home");
+	
+	return (
+		<div className='app bg-gray-20'>
+			<Navbar 
+				selectedPage={selectedPage}
+				setSelectedPage={setSelectedPage}
+			/>
+		</div>)
+	;
 }
 
 export default App;
