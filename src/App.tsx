892d7ae0b9df450de35bdba4a51803a5ba3a8751
posternaka <nik.petrenko97@gmail.com>
2024-05-13 12:02:09 +0300

    developed and styled footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Benefits from '@/scenes/benefits';
+import Footer from '@/scenes/footer';
 import Home from '@/scenes/home';
 import Navbar from '@/scenes/navbar';
 import OurClasses from '@/scenes/ourClasses';
@@ -48,6 +49,7 @@ function App() {
 			<Home setSelectedPage={setSelectedPage} />
 			<Benefits setSelectedPage={setSelectedPage} />
 			<OurClasses setSelectedPage={setSelectedPage} />
+			<Footer />
 		</div>
 	);
 }
