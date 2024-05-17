69a41045e88b39300f037271606fffeff5a7c6c6
posternaka <nik.petrenko97@gmail.com>
2024-05-17 20:38:34 +0300

    home: fixed scroll from home with id

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -17,7 +17,7 @@ const Home = ({ setSelectedPage }: Props) => {
 	const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
 
 	return (
-		<section id='#home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
+		<section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
 			<motion.div
 				className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
 				onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
